c625382215601eda66fc905819c66f826cba54f6
hamid <hamid2262@yahoo.com>
2014-04-26 22:46:52 +0200

    organize layout and create head, header and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
 module ApplicationHelper
 
+	def flash_creator flash
+		message= "";
+		flash.each do |name, msg| 	
+			case name
+				when :notice    
+				  message += "<div class=\"alert alert-success\">#{msg}</div>"	
+				when :warning    
+				  message += "<div class=\"alert alert-warning\">#{msg}</div>"	
+				when :error, :alert
+				  message += "<div class=\"alert alert-danger\">#{msg}</div>"
+				else
+				  message += "<div class=\"alert alert-info\">#{msg}</div>"			
+			end
+	 	end 
+	 	message.html_safe
+	end
+  
 end
